ec8b06bddffa070f9fdb5c23090bc2e944a347ae
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-11-03 22:59:05 +0800

    優化docker scss

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div class="docker">
-    <div class="docker__item ">
-      <span class="material-icons iconfont">home</span>
-      首頁
+  <div>
+    <div class="wrapper">
+      123
     </div>
-    <div class="docker__item">
-      <span class="material-icons iconfont">shopping_cart</span>
-      購物車</div>
-    <div class="docker__item">
-      <span class="material-icons iconfont">toc</span>
-      訂單
-    </div>
-    <div class="docker__item">
-      <span class="material-icons iconfont">perm_identity</span>
-      我的
+    <div class="docker">
+      <div class="docker__item ">
+        <span class="material-icons iconfont">home</span>
+        首頁
+      </div>
+      <div class="docker__item">
+        <span class="material-icons iconfont">shopping_cart</span>
+        購物車</div>
+      <div class="docker__item">
+        <span class="material-icons iconfont">toc</span>
+        訂單
+      </div>
+      <div class="docker__item">
+        <span class="material-icons iconfont">perm_identity</span>
+        我的
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  background: blue;
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -30,16 +43,15 @@
   height: .49rem;
   border-top: 1px solid #f1f1f1;
   // background: green;
-}
-
-.docker__item {
-  flex: 1;
-  text-align: center;
-  .iconfont {
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
     margin: 0.07rem 0 0.02rem 0;
     font-size: .2rem;
     display: flex;
     justify-content: center;
+    }
   }
 }
 
