30e8a5e6c7d072cec14f84beed94b06dd70ef6e6
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-11-03 16:33:27 +0800

    優化docker

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="docker">
     <div class="docker__item ">
-      <span class="material-icons docker__icon">home</span>
+      <span class="material-icons iconfont">home</span>
       首頁
     </div>
     <div class="docker__item">
-      <span class="material-icons docker__icon">shopping_cart</span>
+      <span class="material-icons iconfont">shopping_cart</span>
       購物車</div>
     <div class="docker__item">
-      <span class="material-icons docker__icon">toc</span>
+      <span class="material-icons iconfont">toc</span>
       訂單
     </div>
     <div class="docker__item">
-      <span class="material-icons docker__icon">perm_identity</span>
+      <span class="material-icons iconfont">perm_identity</span>
       我的
     </div>
   </div>
@@ -29,17 +29,18 @@
   width: 100%;
   height: .49rem;
   border-top: 1px solid #f1f1f1;
-  background: green;
+  // background: green;
 }
 
 .docker__item {
   flex: 1;
   text-align: center;
+  .iconfont {
+    margin: 0.07rem 0 0.02rem 0;
+    font-size: .2rem;
+    display: flex;
+    justify-content: center;
+  }
 }
 
-.docker__icon {
-  font-size: 20px;
-  display: flex;
-  justify-content: center;
-}
 </style>
