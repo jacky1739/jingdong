a16468d0c6208ac3d48e719ce6935a9772ac3ac2
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-11-10 21:18:40 +0800

    add login.vue

--- src/views/home/Docker.vue
+++ src/views/home/Docker.vue
@@ -35,7 +35,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../../style/variables';
 @import '../../style/mixins';
 
---
+++ src/views/login/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper">
+    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png" alt="">
+    <div class="wrapper__input">
+        <input class="wrapper__input__coontent" type="text" placeholder="請輸入手機號碼">
+    </div>
+    <div class="wrapper__input">
+        <input class="wrapper__input__coontent" type="password" placeholder="請輸入密碼">
+    </div>
+    <div class="wrapper__login-button">登入</div>
+    <div class="wrapper__login-link">立即註冊</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/variables';
+
+.wrapper {
+  margin: auto;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  &__img {
+      display: block;
+      margin: 0 auto .4rem auto;
+      width: .66rem;
+      height: .66rem;
+  }
+  &__input {
+      height: .48rem;
+      margin: 0 .4rem .16rem .4rem;
+      padding: 0 .16rem;
+      background: #F9F9F9;
+      border: 1px solid rbga(0,0,0,0.10);
+      border-radius: 6px;
+      &__coontent {
+          line-height: .48rem;
+          border: none;
+          outline: none;
+          width: 100%;
+          background: none;
+          font-size: .16rem;
+          color: $content-notice-fontcolor;
+          &::placeholder {
+              color: $content-notice-fontcolor;
+          }
+      }
+  }
+  &__login-button {
+      line-height: .48rem;
+      margin: .32rem .4rem .16rem .4rem;
+      background: #0091FF;
+      box-shadow: 0 .04rem .08rem 0 rgba(0,145,255,0.32);
+      border-radius: 0.04rem;
+      border-radius: 0.04rem;
+      color: #fff;
+      font-size: .16rem;
+      text-align: center;
+  }
+  &__login-link {
+      text-align: center;
+      font-size: .14rem;
+      color: $content-notice-fontcolor;
+  }
+}
+</style>
