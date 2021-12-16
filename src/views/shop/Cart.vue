d6e43e91629a6ba81351ebf7771b09e096e79e5e
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-12-16 18:05:20 +0800

    cart.vue 大概布局完成

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="cart">
     <div class="check">
-      去結帳
+      <div class="check__icon">
+        <img class="check__icon__img" src="http://www.dell-lee.com/imgs/vue3/basket.png" alt="">
+        <!-- <div class="check__icon__tag">1</div> -->
+      </div>
+      <div class="check__info">
+        總計: <span class="check__icon__price">&yen;127</span>
+      </div>
+      <div class="check__btn">
+        去結算
+      </div>
     </div>
   </div>
 </template>
@@ -20,11 +29,30 @@ export default {
   height: .5rem;
 }
 .check {
-  width: .98rem;
-  height: 100%;
-  background: #4FB0F9;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 100%;
+  // background: #4FB0F9;
+  &__icon {
+    width: .84rem;
+    &__img {
+      display: block;
+      margin: .12rem auto;
+      width: .28rem;
+      height: .26rem;
+    }
+  }
+  &__info {
+    flex: 1;
+  }
+  &__btn {
+    width: .98rem;
+    height: 100%;
+    line-height: .5rem;
+    display: flex;
+    color: #FFFFFF;
+    justify-content: center;
+    align-items: center;
+    background: #4FB0F9;
+  }
 }
 </style>
