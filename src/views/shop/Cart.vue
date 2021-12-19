1d01420d8e95649bf099b5cae40400efaf4fce5e
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-12-19 12:54:12 +0800

    cart 切版 ok

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -3,12 +3,12 @@
     <div class="check">
       <div class="check__icon">
         <img class="check__icon__img" src="http://www.dell-lee.com/imgs/vue3/basket.png" alt="">
-        <!-- <div class="check__icon__tag">1</div> -->
+        <div class="check__icon__tag">1</div>
       </div>
       <div class="check__info">
-        總計: <span class="check__icon__price">&yen;127</span>
+        總計: <span class="check__info__price">&yen;127</span>
       </div>
-      <div class="check__btn">
+      <div class="check__btn" @click="clickBtnCheck">
         去結算
       </div>
     </div>
@@ -17,41 +17,70 @@
 
 <script>
 export default {
+  name: 'Cart',
+  setup () {
+    const clickBtnCheck = () => {
+      console.log('click')
+    }
+
+    return { clickBtnCheck }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../style/variables';
+
 .cart {
   position: absolute;
   left: 0;
   right: 0;
   bottom: 0;
-  height: .5rem;
 }
 .check {
   display: flex;
-  height: 100%;
-  // background: #4FB0F9;
+  height: .49rem;
+  border-top: .01rem solid $content-bgColor;
   &__icon {
     width: .84rem;
+    position: relative;
     &__img {
       display: block;
       margin: .12rem auto;
       width: .28rem;
       height: .26rem;
     }
+    &__tag {
+      position: absolute;
+      width: .2rem;
+      height: .2rem;
+      line-height: .16rem;
+      text-align: center;
+      background-color: $highlight-fontColor;
+      border-radius: 50%;
+      font-size: .12rem;
+      top: 0.04rem;
+      right: 0.2rem;
+      color: $bgColor;
+      transform: scale(.5);
+    }
   }
   &__info {
     flex: 1;
+    color: $content-fontcolor;
+    &__price {
+      color: $highlight-fontColor;
+      font-size: .18rem;
+      line-height: .49rem;
+    }
   }
   &__btn {
     width: .98rem;
     height: 100%;
-    line-height: .5rem;
-    display: flex;
-    color: #FFFFFF;
-    justify-content: center;
-    align-items: center;
+    color: $bgColor;
+    line-height: .49rem;
+    text-align: center;
+    font-size: .14rem;
     background: #4FB0F9;
   }
 }
